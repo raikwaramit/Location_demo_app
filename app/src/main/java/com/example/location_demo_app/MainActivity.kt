796906a8816269dc5f566026adea3f7bfd5520fd
Amit Raikwar <raikwar.amit.1603@gmail.com>
2023-06-30 01:22:49 +0530

    [App] Creating demo project with empty activity.

---
+++ app/src/main/java/com/example/location_demo_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.location_demo_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.location_demo_app.ui.theme.Location_demo_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Location_demo_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Location_demo_appTheme {
+        Greeting("Android")
+    }
+}
